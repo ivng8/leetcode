53a9b9803d8b16e7ba305eac7d20b24d692d683d
Ivan Ng <147109497+ivng8@users.noreply.github.com>
2025-01-15 18:08:35 -0500

    Update Two Sum.java

--- Two Sum/Two Sum.java
+++ Two Sum/Two Sum.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> book = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i += 1) {
+            if (book.containsKey(target - nums[i])) {
+                int[] ans = {book.get(target - nums[i]), i};
+                return ans;
+            } else {
+                book.put(nums[i], i);
+            }
+        }
+        int[] ans = {};
+        return ans;
+    }
+}
